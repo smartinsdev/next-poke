0a13cf8d0c72012232b2cf472794f9d7d6124f42
Sinval Martins <sinval.msjunior@gmail.com>
2022-05-21 19:40:14 -0300

    Add the getServerSideProps function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,49 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+export type Pokemon = {
+  url: string
+  name: string
+  img: string
+  type: string
+  height: string
+  weight: string
+}
+
+type Props = {
+  nextPage: string | null
+  previousPage: string | null
+  count: number
+  pokemons: Pokemon[]
+}
+
+const BASE_URL = 'https://pokeapi.co/api/v2/'
+
+const Home: NextPage<Props> = ({ pokemons }) => {
+  console.log(pokemons)
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Next Poke</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+        <h1 className="text-6xl font-bold">Welcome to NextPoke</h1>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {pokemons.map((pokemon, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col items-center justify-center px-2 py-3"
+              >
+                <h3>{pokemon.name}</h3>
+                <a href={pokemon.url}>detail</a>
+              </div>
+            )
+          })}
         </div>
       </main>
 
@@ -81,6 +60,19 @@ const Home: NextPage = () => {
       </footer>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const getPokemons = await fetch(`${BASE_URL}pokemon/`)
+  const data = await getPokemons.json()
+  return {
+    props: {
+      pokemons: data.results,
+      count: data.count,
+      nextPage: data.next,
+      previousPage: data.previous,
+    },
+  }
 }
 
 export default Home
