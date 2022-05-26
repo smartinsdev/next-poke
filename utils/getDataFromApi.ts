b923b11c6c8965aa6f8caad7f5a9818afff182c6
Sinval Martins <sinval.msjunior@gmail.com>
2022-05-26 12:00:06 -0300

    create the function to get pokemon detail

--- utils/getDataFromApi.ts
+++ utils/getDataFromApi.ts
@@ -1,9 +1,15 @@
-import type { Data } from '../types'
+import type { Data, Pokemon } from '../types'
 
 const BASE_URL = 'https://pokeapi.co/api/v2'
 
-export async function getDataFromApi(endpoint: string): Promise<Data> {
+export async function getDataFromApi(endpoint?: string): Promise<Data> {
   const data = await fetch(`${BASE_URL}${endpoint}`)
   const response = await data.json()
+  return response
+}
+
+export async function getPokemonsDetail(urls: string[]): Promise<Pokemon[]> {
+  const data = await Promise.all(urls.map(async (url) => await fetch(url)))
+  const response = await Promise.all(data.map(async (res) => await res.json()))
   return response
 }
