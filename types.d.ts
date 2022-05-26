9c939bc8a5ef5d6f10a28d67934454493f34e00b
Sinval Martins <sinval.msjunior@gmail.com>
2022-05-26 10:11:21 -0300

    create types file

---
+++ types.d.ts
@@ -0,0 +1,24 @@
+export type Data = {
+  next: string | null
+  previous: string | null
+  count: number | null
+  results: [
+    {
+      name: string
+      url: string
+    }
+  ]
+}
+
+export type Pokemon = {
+  id: number
+  name: string
+  height: string
+  weight: string
+  types: [
+    {
+      type: string
+    }
+  ]
+  imageUrl: string
+}
--- utils/getDataFromApi.ts
+++ utils/getDataFromApi.ts
@@ -1,16 +1,6 @@
-const BASE_URL = 'https://pokeapi.co/api/v2'
+import type { Data } from '../types'
 
-type Data = {
-  next: string | null
-  previous: string | null
-  count: number | null
-  results: [
-    {
-      name: string
-      url: string
-    }
-  ]
-}
+const BASE_URL = 'https://pokeapi.co/api/v2'
 
 export async function getDataFromApi(endpoint: string): Promise<Data> {
   const data = await fetch(`${BASE_URL}${endpoint}`)
