0fdb274140ec3bb8679daa03a75fd1dca045ad0e
Sinval Martins <sinval.msjunior@gmail.com>
2022-05-26 12:11:37 -0300

    refactoring the pokemon image

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,11 +1,12 @@
 import { Pokemon } from '../types'
 
 export const Card = ({ name, types, height, weight, id }: Pokemon) => {
+  const pokeIndexImg = ('000' + (id + 1)).slice(-3)
   return (
     <div className="max-w-xs rounded-3xl border bg-white p-10 text-center font-semibold shadow-lg">
       <img
         className="mx-auto mb-3 h-32 w-32 rounded-full shadow-lg"
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+        src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokeIndexImg}.png`}
         alt="product designer"
       />
       <h1 className="text-lg text-gray-700">{name}</h1>
